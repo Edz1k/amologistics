db6faabb227144609403099a7fbc9b463f09186d
dreamprogrammin <mbabazov@list.ru>
2025-07-16 18:20:45 +0500

    visual improvement

--- src/pages/eurodelivery.vue
+++ src/pages/eurodelivery.vue
@@ -60,8 +60,8 @@ const cards = [
       <div class="mx-auto px-4 py-15 flex flex-col items-start z-10 container lg:items-start">
         <div class="h-10 hidden relative lg:block">
           <div class="bg-red-500 h-2 w-[100px] absolute" />
-          <div class="bg-red-500 h-[425px] w-2 absolute" />
-          <div class="bg-red-500 h-2 w-[150px] top-105 absolute" />
+          <div class="bg-red-500 h-[480px] w-2 absolute" />
+          <div class="bg-red-500 h-2 w-[150px] top-120 absolute" />
         </div>
 
         <div class="text-white w-full lg:w-[70%]">
